23287e7e31d497d0f37011bbab3e64e8a3e98005
Osvaldo Gago <opgago@yahoo.com>
2021-06-08 09:31:51 +0100

    Added jpeg and examples on how to use find with regex

--- compress-all.sh
+++ compress-all.sh
@@ -1,2 +1,8 @@
 find . -name '*.jpg' -exec sh -c 'imagemin "$0" "--plugin.webp.quality=82 --plugin.webp.preset=default" > "$0.webp"'  {} \; 
+find . -name '*.jpeg' -exec sh -c 'imagemin "$0" "--plugin.webp.quality=82 --plugin.webp.preset=default" > "$0.webp"'  {} \; 
 find . -name '*.png' -exec sh -c 'imagemin "$0" "--plugin.webp.quality=82 --plugin.webp.preset=default" > "$0.webp"'  {} \; 
+
+
+find /var/www/gp-site-public/wp-content/uploads/sites/**/$(date +%Y)/$(date +%m)/ -regextype awk -regex ".+\.(jpg|png|jpeg)$"
+
+find /var/www/gp-site-public/wp-content/uploads/sites/**/$(date +%Y)/$(date +%m)/ -regextype awk -regex ".+\.(jpg|png|jpeg)$"
